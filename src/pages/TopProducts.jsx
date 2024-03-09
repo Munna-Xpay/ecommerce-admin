ca25902a0907ef869754923514e86e49c3e4544a
Midhunkm17 <you@example.com>
2024-03-09 20:36:48 +0530

    adminlogin

--- src/pages/TopProducts.jsx
+++ src/pages/TopProducts.jsx
@@ -9,44 +9,34 @@ import TopProductsGroceries from '../components/TopProductsGroceries';
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 function TopProducts() {
-  const [currentDateTime, setCurrentDateTime] = useState(new Date());
-
-  useEffect(() => {
-    const timerID = setInterval(() => tick(), 1000);
-
-    return function cleanup() {
-      clearInterval(timerID);
-    };
-  });
-  const tick = () => setCurrentDateTime(new Date());
-
-  //progress bar
-
 
   return (
     <>
       <TopProductsCategory />
-
       <Grid container mt={4}>
-         
+
         <Grid item xs={12} md={5.5}>
-        <Stack direction={'row'} spacing={1}><LaptopMacIcon sx={{ backgroundColor: 'blue', color: 'white', padding: '5px', borderRadius: '3px', height:'20px', width:'20px' }} /> <Typography fontSize={20} fontWeight={'bold'} >Electronics</Typography></Stack>
+          <Stack direction={'row'} spacing={1}><LaptopMacIcon sx={{ backgroundColor: 'blue', color: 'white', padding: '5px', borderRadius: '3px', height: '20px', width: '20px' }} /> <Typography fontSize={20} fontWeight={'bold'} >Electronics</Typography></Stack>
           <TopProductsElectronics />
-          </Grid>
-        <Grid item sx={{marginLeft:{
-          xs:0,
-          md:10
-        }}} xs={12}  md={5.5}>
-        <Stack direction={'row'} spacing={1}><i style={{marginTop:'12px'}} class="fa-solid fa-shirt fa-xl"></i> <Typography fontSize={20} fontWeight={'bold'} >Fashion</Typography></Stack>
+        </Grid>
+        <Grid item sx={{
+          marginLeft: {
+            xs: 0,
+            md: 10
+          }
+        }} xs={12} md={5.5}>
+          <Stack direction={'row'} spacing={1}><i style={{ marginTop: '12px' }} class="fa-solid fa-shirt fa-xl"></i> <Typography fontSize={20} fontWeight={'bold'} >Fashion</Typography></Stack>
           <TopProductsFashion />
-          </Grid>
-          <Grid item sx={{marginLeft:{
-          xs:0,
-          md:0
-        }}} xs={12}  md={5.5}>
-        <Stack direction={'row'} spacing={1}><RestaurantIcon sx={{ backgroundColor: 'black', color: 'white', padding: '5px', borderRadius: '3px', height:'20px', width:'20px' }} /> <Typography fontSize={20} fontWeight={'bold'} >Groceries</Typography></Stack>
+        </Grid>
+        <Grid item sx={{
+          marginLeft: {
+            xs: 0,
+            md: 0
+          }
+        }} xs={12} md={5.5}>
+          <Stack direction={'row'} spacing={1}><RestaurantIcon sx={{ backgroundColor: 'black', color: 'white', padding: '5px', borderRadius: '3px', height: '20px', width: '20px' }} /> <Typography fontSize={20} fontWeight={'bold'} >Groceries</Typography></Stack>
           <TopProductsGroceries />
-          </Grid>
+        </Grid>
       </Grid>
     </>
   )
