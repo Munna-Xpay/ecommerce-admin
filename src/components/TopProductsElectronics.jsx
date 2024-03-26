8dc41e7b2617bf947a55c10c26d49d8250d8e08f
Midhunkm17 <you@example.com>
2024-03-26 17:51:54 +0530

    adminlogin

--- src/components/TopProductsElectronics.jsx
+++ src/components/TopProductsElectronics.jsx
@@ -34,7 +34,7 @@ function TopProductsElectronics() {
       <SwiperSlide style={{padding:'25px'}}>            
         <Box boxShadow={4} borderRadius={2} padding={4} width={150} bgcolor={'white'}>
         <Box border={1} borderColor={'#f1f1f1'} marginBottom={2}> <img width={150} height={130} src={`${BASE_URL}/uploadedFiles/${product?.thumbnail}`} alt="" /></Box> 
-            <Typography fontWeight={'bold'} fontSize={15} textAlign={'center'}>{product.title}</Typography>
+            <Typography fontWeight={'bold'} fontSize={15} textAlign={'center'}>{product.title.length > 30 ? `${product.title.slice(0, 30)}...` : product.title}</Typography>
            <Box textAlign={'center'}><Rating name="read-only" value={product.review_star} readOnly /></Box> 
             <Typography textAlign={'center'} fontWeight={'bold'} fontSize={15} color={'#0dd1b0'}>Available :<span>{product.stockQuantity}</span></Typography>
            <Typography textAlign={'center'} fontWeight={'bold'} fontSize={15} color={'darkblue'}>Already sold :<span>{product.product_sold}</span></Typography>
