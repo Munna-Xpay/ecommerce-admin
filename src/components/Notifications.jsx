dd82890f1f59f0affe19e601c426f6b7f39b27c1
Abhishek7356 <ksabhishek32@gmail.com>
2024-04-09 13:59:52 +0530

    updated implemented notification

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,28 +1,48 @@
 import { Avatar, Box, Button, Divider, Stack, Typography } from '@mui/material'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../redux/baseUrl'
+import { updateNotification } from '../redux/notificationSlice'
+import { editProduct } from '../redux/productSlice'
+import { useNavigate } from 'react-router-dom'
 
 const Notifications = () => {
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
     const notifications = useSelector(state => state.notificationReducer.allNotifications)
+    console.log(notifications)
+
+    const handleAccept = (id, item_id) => {
+        dispatch(editProduct({ id: item_id, data: { isActive: true }, navigate }))
+        dispatch(updateNotification({ id, data: { response: "Accepted" } }))
+    }
+
+    const handleReject = (id) => {
+        dispatch(updateNotification({ id, data: { response: "Rejected" } }))
+    }
 
     const showAllNotifications = notifications.map((item, index) => {
         return (
             <Stack spacing={3} mb={2}>
                 <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
+                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src={`${BASE_URL}/uploadedFiles/${item?.userProPic}`} />
                     <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
+                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>{item?.username}</Typography>
+                        <Typography sx={{ opacity: '.9' }}>{item?.notifyMsg}</Typography>
                         <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
+                            <Typography sx={{ opacity: '.7' }}>{new Date(item?.createdAt).toDateString()}</Typography>
                             <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
+                            <Typography sx={{ opacity: '.7' }}>{item?.type}</Typography>
                         </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
+                        <Typography variant='body2' sx={{ opacity: '.7' }}>{new Date(item?.createdAt).toLocaleTimeString()}</Typography>
+                        {!item.response ? <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
+                            <Button variant='outlined' size='small' color='success' onClick={() => handleAccept(item._id, item.item_id)}>Accept</Button>
+                            <Button variant='outlined' size='small' color='error' onClick={() => handleReject(item._id)}>Decline</Button>
                         </Stack>
+                            :
+                            <Typography color={'secondary'} sx={{ fontWeight: 'bold' }}>- {item.response}</Typography>
+                        }
                     </Stack>
                 </Stack>
                 <Divider />
@@ -33,103 +53,13 @@ const Notifications = () => {
     return (
         <Stack p={2} spacing={2} width={{ xs: '250px', md: 300 }}>
             <Typography gutterBottom variant='h5' sx={{ fontWeight: 'bold' }}>Notifications</Typography>
-
-            <Stack spacing={3} mb={2}>
-                <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
-                    <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
-                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
-                            <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
-                        </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
-                        </Stack>
-                    </Stack>
-                </Stack>
-                <Divider />
-            </Stack>
-            <Stack spacing={3} mb={2}>
-                <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
-                    <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
-                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
-                            <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
-                        </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
-                        </Stack>
-                    </Stack>
-                </Stack>
-                <Divider />
-            </Stack>
-            <Stack spacing={3} mb={2}>
-                <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
-                    <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
-                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
-                            <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
-                        </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
-                        </Stack>
-                    </Stack>
-                </Stack>
-                <Divider />
-            </Stack>
-            <Stack spacing={3} mb={2}>
-                <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
-                    <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
-                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
-                            <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
-                        </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
-                        </Stack>
-                    </Stack>
-                </Stack>
-                <Divider />
-            </Stack>
-            <Stack spacing={3} mb={2}>
-                <Stack direction={'row'} spacing={2}>
-                    <Avatar sx={{ width: '40px', height: '40px' }} variant='rounded' src='https://shop-point.merku.love/assets/4-7f273707.webp' />
-                    <Stack>
-                        <Typography sx={{ fontWeight: 'bold' }} gutterBottom>Mary Wilson</Typography>
-                        <Typography sx={{ opacity: '.9' }}>Leaved a new review</Typography>
-                        <Stack direction={'row'} spacing={1} alignItems={'center'}>
-                            <Typography sx={{ opacity: '.7' }}>9 Hours ago</Typography>
-                            <Box sx={{ width: '17px', height: '17px', borderRadius: '50%', bgcolor: '#00000070' }} />
-                            <Typography sx={{ opacity: '.7' }}>Review</Typography>
-                        </Stack>
-                        <Stack mt={1} direction={'row'} spacing={2} alignItems={'center'}>
-                            <Button variant='outlined' size='small' color='success'>Accept</Button>
-                            <Button variant='outlined' size='small' color='error'>Decline</Button>
-                        </Stack>
-                    </Stack>
-                </Stack>
-                <Divider />
-            </Stack>
-
+            <Divider />
+            {showAllNotifications.length > 0
+                ?
+                showAllNotifications
+                :
+                <Typography color={'secondary'}>Notification is empty !</Typography>
+            }
         </Stack>
     )
 }
